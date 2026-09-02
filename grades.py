b112a932326fdcc68e180589e37ec5d79b4e2b97
Linoy Golani <linoygolani2010@gmail.com>
2026-09-02 18:02:32 +0300

    grades

---
+++ grades.py
@@ -0,0 +1,6 @@
+def calculate_avg(list):
+    sum =0
+    for item in list:
+        sum += item
+    avg = sum / len(list)
+    return avg
---
+++ main.py
@@ -0,0 +1,9 @@
+from grades import calculate_avg
+grades_list = [85, 90, 78, 92, 88]
+average = calculate_avg(grades_list)
+from rich import print
+print(f"The average grade is: {average}")
+
+
+
+
